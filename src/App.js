d4f2405fe9e3b497f04998b78c3096d7135ab95d
Dmitry Khodyrev <dooommm@mail.ru>
2022-08-26 20:56:32 +0800

    add Search components and styles for  react-select

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { Header } from './components/Header';
+import { Search } from './components/Search';
+import { Main } from './components/Main';
+import { Controls } from './components/Controls';
 
 function App() {
   return (
     <>
       <Header />
+      <Main>
+        <Controls />
+      </Main>
     </>
   );
 }
