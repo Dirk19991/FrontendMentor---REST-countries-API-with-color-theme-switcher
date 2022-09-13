0acb33108a9162380a48c0e1385d9fc0d3796dee
Dmitry Khodyrev <dooommm@mail.ru>
2022-09-13 21:51:00 +0800

    add individual cards

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,7 +1,28 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { IoArrowBack } from 'react-icons/io5';
+import { searchByCountry } from '../config';
+import { Button } from '../components/Button';
+import { Info } from '../components/Info';
 
 export const Details = () => {
-  let { name } = useParams();
+  const { name } = useParams();
+  const navigate = useNavigate();
+  const [country, setCountry] = useState(null);
 
-  return <div>Details for {name}</div>;
+  console.log(country);
+
+  useEffect(() => {
+    axios.get(searchByCountry(name)).then(({ data }) => setCountry(data[0]));
+  }, [name]);
+
+  return (
+    <div>
+      <Button onClick={() => navigate(-1)}>
+        <IoArrowBack /> Back
+      </Button>
+      {country && <Info {...country} />}
+    </div>
+  );
 };
